8ef3e353e34dc62d466efc00065af61ed5c62a43
Mark D'Arensbourg <mdarens@gmail.com>
2016-06-23 14:27:35 -0500

    make consumable via npm
    use webpack css-loader to load css
    add linting

--- knockout.pickadate.js
+++ knockout.pickadate.js
@@ -1,32 +1,29 @@
-(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["jquery", "knockout", "picker.date", "picker.time",
-		"pickadate/themes/classic",
-		"pickadate/themes/classic.date",
-		"pickadate/themes/classic.time"], factory);
-	} else {
-		// Browser globals
-		factory($, ko);
-	}
-}(this, function ($, ko) {
-	ko.bindingHandlers.pickadate = {
-		init: function (element, valueAccessor, allBindingsAccessor) {
-			var options = ko.utils.unwrapObservable(valueAccessor()) || {};
-			var valueBinding = allBindingsAccessor.get("value");
-			$(element).pickadate(Object.assign({}, options, {
-				onSet: function() {
-					if (valueBinding) {
-						valueBinding(element.value);
-					}
+var ko = require("knockout");
+var $ = require("jquery");
+require("pickadate/lib/picker");
+require("pickadate/lib/picker.date");
+require("pickadate/lib/picker.time");
+require("css!pickadate/lib/themes/classic.css");
+require("css!pickadate/lib/themes/classic.date.css");
+require("css!pickadate/lib/themes/classic.time.css");
+
+ko.bindingHandlers.pickadate = {
+	init: function (element, valueAccessor, allBindingsAccessor) {
+		var options = ko.utils.unwrapObservable(valueAccessor()) || {};
+		var valueBinding = allBindingsAccessor.get("value");
+		$(element).pickadate(Object.assign({}, options, {
+			onSet: function() {
+				if (valueBinding) {
+					valueBinding(element.value);
 				}
-			}));
-		}
-	};
-	ko.bindingHandlers.pickatime = {
-		init: function (element, valueAccessor, allBindingsAccessor) {
-			var options = ko.utils.unwrapObservable(valueAccessor()) || {};
-			$(element).pickatime(options);
-		}
-	};
-}));
+			}
+		}));
+	}
+};
+
+ko.bindingHandlers.pickatime = {
+	init: function (element, valueAccessor) {
+		var options = ko.utils.unwrapObservable(valueAccessor()) || {};
+		$(element).pickatime(options);
+	}
+};
